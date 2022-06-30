d6638fb3a3e439edd6b6ede9f50aeadb780dc3f2
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-06-30 10:45:23 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 
 /**
 * main - Prints "and that piece of art is useful"
+
 * - Dora Korpar, 2015-10-19",            
 * followed by a new line, to standard error.
 * Return: Always 1.
@@ -16,8 +17,8 @@ int main(void)
 
 {
 
-		write(2,
-   "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2,
+ "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 }
