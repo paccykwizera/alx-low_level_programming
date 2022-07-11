7b7371c5c99ff7f41950adacd3a90a6fc0a43ead
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-07-11 11:43:29 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line skipping 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,14 @@ void print_most_numbers(void)
 int x;
 for (x = 0 ; x <= 9; x++)
 {
-if (x == 2 || x == 4)
-{
-continue;
-}
-else
-{																	_putchar(x + '0');															
-}
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+		_putchar(x + '0');															
+	}
 }
 _putchar('\n');
 }
