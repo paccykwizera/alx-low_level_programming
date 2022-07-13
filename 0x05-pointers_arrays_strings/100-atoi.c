1730735847b0fceaccb2ead7c57717ecdef9ebb8
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-07-13 12:46:51 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,12 +4,12 @@
 
 
  /**
-  * _atoi - Convert a string to an integer.
+  *atoi - Convert a string to an integer.
   * @s: The pointer to convert
   * Return: A integer
   */
 
- int _atoi(char *s)
+int _atoi(char *s)
 {
 int c = 0;
 unsigned int ni = 0;
