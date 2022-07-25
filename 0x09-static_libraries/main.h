a5a8c2c3e92f1288e3891a80b4c3e6d8ef624a06
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-07-25 14:47:11 -0700

    update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +41,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+#endif
