361a2680d2f8b2f0b3110a565816e7431956f430
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-07-26 18:01:40 -0700

    program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,12 +15,12 @@
 
 int main(int argc, char *argv[])
 {
-int 'n1 = 0, n2' = 0;
+int n1 = 0, n2 = 0;
 if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", 'n1 *n2');
+printf("%d\n", n1 *n2);
 }
 else
 {
