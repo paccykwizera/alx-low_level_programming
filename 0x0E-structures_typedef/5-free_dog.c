452874f1023c1e39f8ce20848a0b35f9a082b374
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-08-02 08:54:26 -0700

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include "dog.h"
+
+
+
+/**
+ * free_dog - ...
+ * @d: ...
+ * Return: always 0
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
+}
