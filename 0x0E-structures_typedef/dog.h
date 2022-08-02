993ff3f0dfa8d0626237aaf26070a17a3dd3272f
“paccykwizera” <pacy0234@gmail.com@gmail.com>
2022-08-02 06:48:08 -0700

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 #define _HEADER_
 
 
-
 /**
  * struct dog - Dog attributes
  * @name: The name of the dog.
@@ -20,12 +19,11 @@ char *owner;
 };
 
 
-
 /**
  * dog_t - Typedef for dog structure
  */
-
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
