5b75cd751c2510159e07ec3646c546d46d25c869
paccykwizera <pacy0234@gmail.com>
2022-08-21 13:42:32 -0700

    update header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
-#ifndef _S_LINK_LISTS_
+#ifndef _LISTS_H_
 
-#define _S_LINK_LISTS_
+#define _LISTS_H_
 
 
+
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -44,12 +46,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 listint_t *reverse_listint(listint_t **head);
 
-listint_t *find_listint_loop(listint_t *head);
-
 size_t print_listint_safe(const listint_t *head);
 
 size_t free_listint_safe(listint_t **h);
 
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+
+
+#endif /* _LISTS_H_ */
